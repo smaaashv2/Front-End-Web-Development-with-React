fd6b103480ba33e0fad134501ae01ba224147ded
Viraj <desaiviraj28@gmail.com>
2020-05-18 20:18:08 +0530

    Single page application part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 	const RenderDish = (dish) => {
         if (dish != null) {
@@ -15,10 +16,11 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
                 </div>
             );
         }
-        else
+        else {
             return(
                 <div />
             );
+        }
     }
 
     const RenderComments = (comments) => {
@@ -40,24 +42,35 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
                 </div>
         	);
     	}
-    	else
+    	else {
     		return(
     			<div/>
     		);
+        }
     }
 
 
     const Dishdetail = (props) => {
         if (props.dish == null) {
-            return (<div></div>)
+            return (<div></div>);
         }
-        const dishItem = RenderDish(props.dish)
-        const commentItem = RenderComments(props.dish.comments)
+        const renderDish = RenderDish(props.dish)
+        const renderComments = RenderComments(props.comments)
         return (
         	<div className="container">
-            	<div className="row">
-                	{dishItem}
-                	{commentItem}
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    {renderDish}
+                    {renderComments}
                 </div>
             </div>
         );
