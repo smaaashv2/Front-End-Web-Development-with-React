5fb39f4d50988f7fc87ba619f256992496aeaaed
Viraj <desaiviraj28@gmail.com>
2020-05-21 14:47:19 +0530

    redux-thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -26,7 +27,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
-        alert(JSON.stringify(values));
+        //alert(JSON.stringify(values));
     }
     render(){
         return (
@@ -142,7 +143,25 @@ function RenderComments(comments, addComment, dishId) {
 
 
 const Dishdetail = (props) => {
-    if (props.dish == null) {
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.dish == null) {
         return (<div></div>);
     }
     const renderDish = RenderDish(props.dish);
